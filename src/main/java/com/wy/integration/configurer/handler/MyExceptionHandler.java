c415a806bb85a57e110506473a746b8b6daf19db
EdwardWangYan <976452322@qq.com>
2018-06-24 22:00:41 +0800

    Add the as Exception

--- src/main/java/com/wy/integration/configurer/handler/MyExceptionHandler.java
+++ src/main/java/com/wy/integration/configurer/handler/MyExceptionHandler.java
@@ -7,18 +7,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class MyExceptionHandler {
 
     @ExceptionHandler(BizException.class)
-    public ApiResult defultExcepitonHandler(HttpServletRequest request, Exception e) {
+    public Map defultExcepitonHandler(HttpServletRequest request, Exception e) {
         e.printStackTrace();
+        Map map = new HashMap();
         if(e instanceof BizException) {
             BizException businessException = (BizException)e;
-            return ResultUtil.error(businessException.getCode(), businessException.getMessage());
+            map.put("code", businessException.getCode());
+            map.put("msg", businessException.getMessage());
+        }else{
+            map.put("code", -1);
+            map.put("msg", "系统异常");
         }
         //未知错误
-        return ResultUtil.error(-1, "系统异常：\\n"+e);
+        return map;
     }
 }
